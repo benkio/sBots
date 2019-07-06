dbc854c45ae36e3a87017a4e2e3ea1df9488de90
Benkio <benkio89@gmail.com>
2019-07-06 01:11:14 +0100

    refactor the collections. Add new gifs/audio

--- richardPHJBensonBot/src/main/scala/root/RichardPHJBensonBot.scala
+++ richardPHJBensonBot/src/main/scala/root/RichardPHJBensonBot.scala
@@ -28,21 +28,22 @@ object MessageMatches {
     */
   def getHandler(keys : List[String],
     messageText : String,
-    handler : RichardPHJBensonBot.MessageHandler,
-    matcher : MessageMatches = ContainsOnce) : Option[RichardPHJBensonBot.MessageHandler] = matcher match {
+    handler : MessageHandler,
+    matcher : MessageMatches = ContainsOnce) : Option[MessageHandler] = matcher match {
     case ContainsOnce if (keys.exists(k => messageText.toLowerCase() contains k)) => Some(handler)
     case ContainsAll if (keys.forall(k => messageText.toLowerCase() contains k)) => Some(handler)
     case _ => None
   }
 }
 
+//wrapper around the handler
+case class MessageHandler(handler: Message => Future[Message])
+
 object RichardPHJBensonBot extends TelegramBot
     with Polling
     with Commands
     with ChatActions
     with Messages {
-
-  type MessageHandler = Message => Future[Message]
 
   // Configuration Stuff //////////////////////////////////////////////////////
   lazy val token = scala.util.Properties
@@ -81,85 +82,93 @@ object RichardPHJBensonBot extends TelegramBot
   // Message Replies ////////////////////////////////////////////////////////////
 
 
-  val messageRepliesAudio = List(
-    (List("maledetto"), (m : Message)                       => sendAudioBenson("maledetto.mp3")(m), ContainsOnce),
-    (List("aiuto", "aiutatemi", "aiutatelo"), (m : Message) => sendAudioBenson("aiuto.mp3")(m), ContainsOnce),
-    (List("ritornata", "ritornato"), (m : Message)          => sendAudioBenson("ritornata.mp3")(m), ContainsOnce),
-    (List("ma che cazzo sto dicendo"), (m : Message)        => sendAudioBenson("machecazzostodicendo.mp3")(m), ContainsAll),
-    (List("poveri cretini"), (m : Message)                  => sendAudioBenson("povericretini.mp3")(m), ContainsAll),
-    (List("ho capito"), (m : Message)                       => sendAudioBenson("hocapito.mp3")(m), ContainsAll),
-    (List("feelings"), (m : Message)                        => sendAudioBenson("feelings.mp3")(m), ContainsAll),
-    (List("avete capito"), (m : Message)                    => sendAudioBenson("avetecapito.mp3")(m), ContainsAll),
-    (List("due ossa"), (m : Message)                        => sendAudioBenson("dueossa.mp3")(m), ContainsAll),
-    (List("schifo"), (m : Message)                          => sendAudioBenson("schifo.mp3")(m), ContainsAll),
-    (List("pappalardo"), (m : Message)                      => sendAudioBenson("pappalardo.mp3")(m), ContainsAll),
-    (List("attenzione"), (m : Message)                      => sendAudioBenson("attenzione.mp3")(m), ContainsAll)
-  )
+  val messageRepliesAudio : List[(List[String], MessageHandler, MessageMatches)] = List(
+    (List("maledetto")                            , "maledetto.mp3"            , ContainsOnce) ,
+    (List("aiuto" , "aiutatemi" , "aiutatelo")    , "aiuto.mp3"                , ContainsOnce) ,
+    (List("ritornata" , "ritornato")              , "ritornata.mp3"            , ContainsOnce) ,
+    (List("merda")                                , "merda.mp3"                , ContainsOnce) ,
+    (List("ma che cazzo sto dicendo")             , "machecazzostodicendo.mp3" , ContainsAll)  ,
+    (List("poveri cretini")                       , "povericretini.mp3"        , ContainsAll)  ,
+    (List("ho capito")                            , "hocapito.mp3"             , ContainsAll)  ,
+    (List("feelings")                             , "feelings.mp3"             , ContainsAll)  ,
+    (List("avete capito")                         , "avetecapito.mp3"          , ContainsAll)  ,
+    (List("due ossa")                             , "dueossa.mp3"              , ContainsAll)  ,
+    (List("schifo")                               , "schifo.mp3"               , ContainsAll)  ,
+    (List("pappalardo")                           , "pappalardo.mp3"           , ContainsAll)  ,
+    (List("attenzione")                           , "attenzione.mp3"           , ContainsAll)  ,
+    (List("pollo")                                , "pollo.mp3"                , ContainsAll)
+  ).map {
+    case (words, mp3file, matcher) =>
+      (words, MessageHandler((m : Message) => sendAudioBenson(mp3file)(m)), matcher)
+  }
 
   val messageRepliesGifs = List(
-    (List("bravo"), (m : Message)                                                                  => sendGifBenson("bravo.gif")(m), ContainsOnce),
-    (List("masgus", "ma sgus", "ma scusa"), (m : Message)                                          => sendGifBenson("masgus.gif")(m), ContainsOnce),
-    (List("capolavoro"), (m : Message)                                                             => sendGifBenson("capolavoro.gif")(m), ContainsOnce),
-    (List("mare di cazzate"), (m : Message)                                                        => sendGifBenson("nonépossibile.gif")(m), ContainsOnce),
-    (List("porca miseria"), (m : Message)                                                          => sendGifBenson("porcamiseria.gif")(m), ContainsOnce),
-    (List("schifoso"), (m : Message)                                                               => sendGifBenson("schifoso.gif")(m), ContainsOnce),
-    (List("fammi questa cortesia"), (m : Message)                                                  => sendGifBenson("fammiquestacortesia.gif")(m), ContainsOnce),
-    (List("ma vattene affanculo"), (m : Message)                                                   => sendGifBenson("mavatteneaffanculo.gif")(m), ContainsOnce),
-    (List("rimpinzati", "cioccolata"), (m : Message)                                               => sendGifBenson("rimpinzati.gif")(m), ContainsOnce),
-    (List("facendo soffrire"), (m : Message)                                                       => sendGifBenson("facendosoffrire.gif")(m), ContainsOnce),
-    (List("mi sto sentendo male"), (m : Message)                                                   => sendGifBenson("mistosentendomale.gif")(m), ContainsOnce),
-    (List("mi fa stare male"), (m : Message)                                                       => sendGifBenson("mifastaremale.gif")(m), ContainsOnce),
-    (List("lunghezza d'onda"), (m : Message)                                                       => sendGifBenson("lunghezzadonda.gif")(m), ContainsOnce),
-    (List("delirio"), (m : Message)                                                                => sendGifBenson("delirio.gif")(m), ContainsOnce),
-    (List("grazie", "gianni"), (m : Message)                                                       => sendGifBenson("grazie.gif")(m), ContainsOnce),
-    (List("non mi sta bene"), (m : Message)                                                        => sendGifBenson("nonmistabene.gif")(m), ContainsOnce),
-    (List("paradosso"), (m : Message)                                                              => sendGifBenson("paradosso.gif")(m), ContainsOnce),
-    (List("deficienza", "deficiente"), (m : Message)                                               => sendGifBenson("deficienza.gif")(m), ContainsOnce),
-    (List("querelare"), (m : Message)                                                              => sendGifBenson("querelare.gif")(m), ContainsOnce),
-    (List("pigliando per il culo", "prendendo per il culo"), (m : Message)                         => sendGifBenson("pigliandoperilculo.gif")(m), ContainsOnce),
-    (List("non voglio nessuno"), (m : Message)                                                     => sendGifBenson("nonvoglionessuno.gif")(m), ContainsOnce),
-    (List("cesso", "bagno"), (m : Message)                                                         => sendGifBenson("alcesso.gif")(m), ContainsOnce),
-    (List("ciao a tutti", "ciao", "come state", "belle gioie"), (m : Message)                      => sendGifBenson("ciaocomestate.gif")(m), ContainsOnce),
-    (List("non manca niente", "tutto"), (m : Message)                                              => sendGifBenson("nonmancaniente.gif")(m), ContainsOnce),
-    (List("mettiti in fila"), (m : Message)                                                        => sendGifBenson("mettitiinfila.gif")(m), ContainsOnce),
-    (List("non male"), (m : Message)                                                               => sendGifBenson("nonmale.gif")(m), ContainsOnce),
-    (List("si sente"), (m : Message)                                                               => sendGifBenson("sisente.gif")(m), ContainsOnce),
-    (List("non ci credete", "stronzata", "stronzate"), (m : Message)                               => sendGifBenson("noncicredete.gif")(m), ContainsOnce),
-    (List("sto diventando pazzo", "sto diventando matto"), (m : Message)                           => sendGifBenson("stodiventandopazzo.gif")(m), ContainsOnce),
-    (List("mi pare logico", "calcolo", "matematica"), (m : Message)                                => sendGifBenson("miparelogico.gif")(m), ContainsOnce),
-    (List("me ne vado"), (m : Message)                                                             => sendGifBenson("menevado.gif")(m), ContainsOnce),
-    (List("ti dovresti vergognare", "vergogna"), (m : Message)                                     => sendGifBenson("paradosso.gif")(m), ContainsOnce),
-    (List("sorca", "lecciso", "figa"), (m : Message)                                               => sendGifBenson("sorcalecciso.gif")(m), ContainsOnce),
-    (List("vergogna"), (m : Message)                                                               => sendGifBenson("vergogna.gif")(m), ContainsOnce),
-    (List("ammazza", "frocio"), (m : Message)                                                      => sendGifBenson("frocio.gif")(m), ContainsOnce),
-    (List("non li sopporto"), (m : Message)                                                        => sendGifBenson("nonlisopporto.gif")(m), ContainsOnce),
-    (List("ok", "good", "show", "friends"), (m : Message)                                          => sendGifBenson("okgoodshowfriends.gif")(m), ContainsOnce),
-    (List("infernali"), (m : Message)                                                              => sendGifBenson("infernali.gif")(m), ContainsOnce),
-    (List("deluso", "mi sta deludendo"), (m : Message)                                             => sendGifBenson("deluso.gif")(m), ContainsOnce),
-    (List("spaventare"), (m : Message)                                                             => sendGifBenson("tidevispaventare.gif")(m), ContainsOnce),
-    (List("chi cazzo sei"), (m : Message)                                                          => sendGifBenson("chicazzosei.gif")(m), ContainsOnce),
-    (List("non me ne fotte", "chissenefrega", "non mi importa", "non mi interessa"), (m : Message) => sendGifBenson("nonmenefotte.gif")(m), ContainsOnce),
-    (List("feste"), (m : Message)                                                                  => sendGifBenson("feste.gif")(m), ContainsOnce),
-    (List("ostina", "foto vecchie", "ostini"), (m : Message)                                       => sendGifBenson("ostina.gif")(m), ContainsOnce),
-    (List("sapere", "sapevo", "sapesse", "saperlo", "non lo so", "aristotele"), (m : Message)      => sendGifBenson("sodinonsapere.gif")(m), ContainsOnce),
-    (List("vecchio", "vecchia"), (m : Message)                                                     => sendGifBenson("vecchio.gif")(m), ContainsOnce),
-    (List("pagare", "paga", "soldi", "bollette", "tasse",  "bolletta", "tassa"),(m : Message)     => sendGifBenson("cacciaisoldi.gif")(m), ContainsOnce)
-  )
+    (List("bravo"                                                                  ), "bravo.gif",              ContainsOnce ),
+    (List("capolavoro"                                                             ), "capolavoro.gif",         ContainsOnce ),
+    (List("mare di cazzate"                                                        ), "nonépossibile.gif",      ContainsOnce ),
+    (List("porca miseria"                                                          ), "porcamiseria.gif",       ContainsOnce ),
+    (List("schifoso"                                                               ), "schifoso.gif",           ContainsOnce ),
+    (List("ma vattene affanculo"                                                   ), "mavatteneaffanculo.gif", ContainsOnce ),
+    (List("rimpinzati", "cioccolata"                                               ), "rimpinzati.gif",         ContainsOnce ),
+    (List("facendo soffrire"                                                       ), "facendosoffrire.gif",    ContainsOnce ),
+    (List("mi sto sentendo male"                                                   ), "mistosentendomale.gif",  ContainsOnce ),
+    (List("mi fa stare male"                                                       ), "mifastaremale.gif",      ContainsOnce ),
+    (List("lunghezza d'onda"                                                       ), "lunghezzadonda.gif",     ContainsOnce ),
+    (List("delirio"                                                                ), "delirio.gif",            ContainsOnce ),
+    (List("paradosso"                                                              ), "paradosso.gif",          ContainsOnce ),
+    (List("deficienza", "deficiente"                                               ), "deficienza.gif",         ContainsOnce ),
+    (List("querelare"                                                              ), "querelare.gif",          ContainsOnce ),
+    (List("pigliando per il culo", "prendendo per il culo"                         ), "pigliandoperilculo.gif", ContainsOnce ),
+    (List("non voglio nessuno"                                                     ), "nonvoglionessuno.gif",   ContainsOnce ),
+    (List("cesso", "bagno"                                                         ), "alcesso.gif",            ContainsOnce ),
+    (List("ciao a tutti", "ciao", "come state", "belle gioie"                      ), "ciaocomestate.gif",      ContainsOnce ),
+    (List("non manca niente", "tutto"                                              ), "nonmancaniente.gif",     ContainsOnce ),
+    (List("in fila"                                                                ), "mettitiinfila.gif",      ContainsOnce ),
+    (List("non male"                                                               ), "nonmale.gif",            ContainsOnce ),
+    (List("si sente"                                                               ), "sisente.gif",            ContainsOnce ),
+    (List("non ci credete", "stronzata", "stronzate"                               ), "noncicredete.gif",       ContainsOnce ),
+    (List("sto diventando pazzo", "sto diventando matto"                           ), "stodiventandopazzo.gif", ContainsOnce ),
+    (List("mi pare logico", "calcolo", "matematica"                                ), "miparelogico.gif",       ContainsOnce ),
+    (List("ti dovresti vergognare", "vergogna"                                     ), "paradosso.gif",          ContainsOnce ),
+    (List("sorca", "lecciso", "figa"                                               ), "sorcalecciso.gif",       ContainsOnce ),
+    (List("vergogna"                                                               ), "vergogna.gif",           ContainsOnce ),
+    (List("ammazza", "frocio"                                                      ), "frocio.gif",             ContainsOnce ),
+    (List("non li sopporto"                                                        ), "nonlisopporto.gif",      ContainsOnce ),
+    (List("ok", "good", "show", "friends"                                          ), "okgoodshowfriends.gif",  ContainsOnce ),
+    (List("deluso", "mi sta deludendo"                                             ), "deluso.gif",             ContainsOnce ),
+    (List("chi cazzo sei"                                                          ), "chicazzosei.gif",        ContainsOnce ),
+    (List("non me ne fotte", "chissenefrega", "non mi importa", "non mi interessa" ), "nonmenefotte.gif",       ContainsOnce ),
+    (List("feste"                                                                  ), "feste.gif",              ContainsOnce ),
+    (List("ostina", "foto vecchie", "ostini"                                       ), "ostina.gif",             ContainsOnce ),
+    (List("sapere", "sapevo", "sapesse", "saperlo", "non lo so", "aristotele"      ), "sodinonsapere.gif",      ContainsOnce ),
+    (List("vecchio", "vecchia"                                                     ), "vecchio.gif",            ContainsOnce ),
+    (List("pagare", "paga", "soldi", "bollette", "tasse",  "bolletta", "tassa"     ),"cacciaisoldi.gif",        ContainsOnce ),
+    (List("venite qua"                                                             ), "venitequa.gif",          ContainsOnce ),
+    (List("sarete coglioni voi"                                                    ), "saretecoglionivoi.gif",  ContainsOnce )
+  ) map {
+    case (words, gifFile, matcher) =>
+      (words, MessageHandler((m : Message) => sendGifBenson(gifFile)(m)), matcher)
+  }
 
   val messageRepliesSpecial = List(
-    (List("basta"), (m : Message)           => {
-      sendGifBenson("basta.gif")(m)
-      sendAudioBenson("basta.mp3")(m)
-    }, ContainsOnce),
-    (List("questa volta no"), (m : Message) => {
-      sendAudioBenson("questavoltano.mp3")(m)
-      sendGifBenson("questavoltano.gif")(m)
-    }, ContainsAll),
-    (List("napoli"), (m : Message)          => {
-      sendAudioBenson("vivaNapoli.mp3")(m)
-      sendGifBenson("vivanapoli.gif")(m)
-    }, ContainsOnce),
-  )
+    (List("basta"                         ), "basta.gif", "basta.mp3",                             ContainsOnce ),
+    (List("spaventare"                    ), "tidevispaventare.gif", "tidevispaventare.mp3",       ContainsOnce ),
+    (List("questa volta no"               ), "questavoltano.mp3", "questavoltano.gif",             ContainsAll  ),
+    (List("masgus", "ma sgus", "ma scusa" ), "masgus.gif", "masgus.mp3",                           ContainsOnce ),
+    (List("grazie", "gianni"              ), "grazie.gif", "grazie.mp3",                           ContainsOnce ),
+    (List("me ne vado"                    ), "menevado.gif", "menevado.mp3",                       ContainsOnce ),
+    (List("incontr"                       ), "incontrateperstrada.gif", "incontrateperstrada.mp3", ContainsOnce ),
+    (List("infernali"                     ), "infernali.gif", "infernali.mp3",                     ContainsOnce ),
+    (List("fammi questa cortesia"         ), "fammiquestacortesia.gif", "fammiquestacortesia.mp3", ContainsOnce ),
+    (List("non mi sta bene"               ), "nonmistabene.gif", "nonmistabene.mp3",               ContainsOnce ),
+    (List("napoli"                        ), "vivaNapoli.mp3", "vivanapoli.gif",                   ContainsOnce )
+  ) map {
+    case (words, gifFile, mp3file, matcher) =>
+      (words, MessageHandler((m : Message) => {
+        sendAudioBenson(mp3file)(m)
+        sendGifBenson(gifFile)(m)
+      }), matcher)
+  }
 
   // Map contains the list of keywords to match, the related messageHandler and
   // the Message matches.
@@ -170,7 +179,7 @@ object RichardPHJBensonBot extends TelegramBot
     message.text.map { m =>
       messageReplies
         .flatMap(t => MessageMatches.getHandler(t._1, m, t._2, t._3).toList)
-        .foreach(_(message))
+        .foreach(_.handler(message))
     }
   )
 }
