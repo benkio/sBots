a7054cb5488c3484eb3e1ee7748431d5a339762a
Enrico Benini <benkio89@gmail.com>
2019-09-02 22:46:05 +0100

    Add Arivado

--- richardPHJBensonBot/src/main/scala/root/RichardPHJBensonBot.scala
+++ richardPHJBensonBot/src/main/scala/root/RichardPHJBensonBot.scala
@@ -173,6 +173,7 @@ object RichardPHJBensonBot extends TelegramBot
       "non me ne frega un cazzo"                ), "escerto.gif",              ContainsOnce),
     (List("sorriso", emoji":smile:"             ), "sorriso.gif",              ContainsOnce),
     (List("lasciami in pace", "stronz"          ), "lasciamiinpace.gif",       ContainsOnce),
+    (List("sono arrivato", "arivado", "piacere" ), "arivado.gif",              ContainsOnce),
     (List("pure bona"                           ), "bona.gif",              ContainsOnce),
     (List("mortacci vostri"                     ), "mortaccivostri.gif",       ContainsOnce),
     (List(" danza", "macabra", " ball"          ), "danzamacabra.gif",         ContainsOnce),
