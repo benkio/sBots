fc13d86688cad6865682bc09461f52bf019a155d
Enrico Benini <benkio89@gmail.com>
2019-09-06 19:30:42 +0100

    Unused Import

--- richardPHJBensonBot/src/test/scala/root/RichardPHJBensonBotSpec.scala
+++ richardPHJBensonBot/src/test/scala/root/RichardPHJBensonBotSpec.scala
@@ -3,7 +3,7 @@ package root
 import org.scalatest._
 import java.nio.file.Files
 
-import root.infrastructure.model.{GifFile, MediaFile, Mp3File}
+import root.infrastructure.model.MediaFile
 
 class RichardPHJBensonBotSpec extends WordSpec {
 
