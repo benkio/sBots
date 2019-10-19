5464c88aace05c48515ef6516c596330229aec8a
Enrico Benini <benkio89@gmail.com>
2019-10-20 00:22:03 +0100

    Upgrade the lunch.sh

--- lunch.sh
+++ lunch.sh
@@ -1,18 +1,47 @@
-cd ./telegrambotinfrastructure/
+# Script for sarting the bots ##################################################
+#
+# This script will always step into the single projects and run a test + run
+#
+# -r flag the script will package the common library used into the bots
+################################################################################
 
-sbt assembly
+echo "################################################################################"
+echo "#                     Welcome to the bot starting script                       #"
+echo "#                                                                              #"
+echo "################################################################################"
+RECOMPILE=false
 
-cp ./bin/TelegramBotInfrastructure-0.0.1.jar ../richardPHJBensonBot/lib/
-mv ./bin/TelegramBotInfrastructure-0.0.1.jar ../calandroBot/lib/
+while getopts dr option
+do
+    case "${option}"
+    in
+        r) RECOMPILE=true
+           echo "selected the Recompile option";;
+        ?) echo "no options selected"
+    esac
+done
 
-cd ..
+if [ "$RECOMPILE" = true ] ;
+then
+    echo "Recompiling the infrastructure"
+    cd ./telegrambotinfrastructure/
 
-ls -l ./calandroBot//lib/ #to see the content of the lib folder
-ls -l ./richardPHJBensonBot/lib/ #to see the content of the lib folder
+    sbt assembly
 
+    echo "Moving the library to the bots lib folders"
+    cp ./bin/TelegramBotInfrastructure-0.0.1.jar ../richardPHJBensonBot/lib/
+    mv ./bin/TelegramBotInfrastructure-0.0.1.jar ../calandroBot/lib/
+    ls -l ../calandroBot/lib/ #to see the content of the lib folder
+    ls -l ../richardPHJBensonBot/lib/ #to see the content of the lib folder
+
+    cd ..
+    echo "Finish the recompilation of the infrastructure"
+fi
+
+echo "test & run"
 
 (cd ./calandroBot/; sbt test) &
 (cd ./richardPHJBensonBot/; sbt test) &
 (cd ./calandroBot/; sbt run) &
-(cd ./djJacobBot/; sbt run) &
+# (cd ./djJacobBot/; sbt run) &
 (cd ./richardPHJBensonBot/; sbt run)
