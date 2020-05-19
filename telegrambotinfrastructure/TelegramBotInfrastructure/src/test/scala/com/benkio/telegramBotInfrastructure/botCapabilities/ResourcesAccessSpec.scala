1a6f8f107d6b37abc6af7fd0e42bcbd63bf546aa
Enrico Benini <ebenini@mdsol.com>
2020-05-19 17:27:57 +0100

    ResourcesAccessSpec

--- telegrambotinfrastructure/TelegramBotInfrastructure/src/test/scala/com/benkio/telegramBotInfrastructure/botCapabilities/ResourcesAccessSpec.scala
+++ telegrambotinfrastructure/TelegramBotInfrastructure/src/test/scala/com/benkio/telegramBotInfrastructure/botCapabilities/ResourcesAccessSpec.scala
@@ -1,16 +1,19 @@
 package com.benkio.telegramBotInfrastructure.botCapabilities
 
 import org.scalatest._
+import java.nio.file._
 
 class ResourcesAccessSpec extends WordSpec with Matchers {
 
   val resourcesAccess = new ResourcesAccess {}
+  val rootPath = Paths.get("").toAbsolutePath()
+  val testfile = "testFile"
 
   "ResourcesAccess - buildPath" should {
     "return the expected path" when {
       "the filename is provided" in {
-        val result = resourcesAccess.buildPath("testFile")
-        println(s"result: $result")
+        val result = resourcesAccess.buildPath(testfile)
+        result.toString shouldEqual (rootPath + "/src/main/resources/" + testfile)
       }
     }
   }
