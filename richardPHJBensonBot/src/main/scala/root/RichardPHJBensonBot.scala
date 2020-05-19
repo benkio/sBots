f5b0dea8428068eae951d49efd7d9f0dbea78bf1
Enrico Benini <benkio89@gmail.com>
2020-05-19 20:11:53 +0100

    non mi menzionare questa parola

--- richardPHJBensonBot/src/main/scala/root/RichardPHJBensonBot.scala
+++ richardPHJBensonBot/src/main/scala/root/RichardPHJBensonBot.scala
@@ -104,6 +104,7 @@ object RichardPHJBensonBot extends BotSkeleton {
   )
 
   val messageRepliesGifsData: List[ReplyBundleMessage] = List(
+    ReplyBundleMessage(TextTrigger(List(StringTextTriggerValue("menzion"))), List(MediaFile("nonMiMenzionareQuestaParola.gif"))),
     ReplyBundleMessage(TextTrigger(List(StringTextTriggerValue("hollywood"))), List(MediaFile("hollywood.gif"))),
     ReplyBundleMessage(TextTrigger(List(StringTextTriggerValue("chi è"))), List(MediaFile("questaPersonaScusate.gif"))),
     ReplyBundleMessage(
