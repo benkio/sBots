02438d5662d7017fcbce564753f47aceff65ff9f
Enrico Benini <benkio89@gmail.com>
2021-09-21 23:41:58 +0100

    spaces

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
     val catsVersion           = "2.6.1"
     val catsEffectVersion     = "2.5.4"
-    val scalaTestVersion      = "3.2.10"    
+    val scalaTestVersion      = "3.2.10"
     val telegramiumVersion    = "6.53.0"
     val sqliteJdbcVersion     = "3.36.0.3"
     val randomVersion         = "0.0.2"
