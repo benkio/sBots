6e409b21f4faf6d6f0c2e7f3e7f78f5efed08958
Enrico Benini <benkio89@gmail.com>
2022-07-05 14:49:21 +0100

    Add `Il Bongo`

--- richardPHJBensonBot/src/main/scala/com/benkio/richardphjbensonbot/data/Gif.scala
+++ richardPHJBensonBot/src/main/scala/com/benkio/richardphjbensonbot/data/Gif.scala
@@ -1008,6 +1008,15 @@ object Gif {
         MediaFile("rphjb_VabbeProblema.gif"),
       ),
     ),
+    ReplyBundleMessage(
+      TextTrigger(
+        StringTextTriggerValue("il bongo"),
+        StringTextTriggerValue("non esiste un basso più pontente al mondo"),
+      ),
+      List(
+        MediaFile("rphjb_IlBongo.gif")
+      )
+    )
   )
 
 }
