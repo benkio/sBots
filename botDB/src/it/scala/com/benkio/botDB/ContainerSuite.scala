cfe3a9d2889b6193ca57f167d014d4da61ef8e0f
Enrico Benini <benkio89@gmail.com>
2022-07-16 17:57:28 +0100

    format

--- botDB/src/it/scala/com/benkio/botDB/ContainerSuite.scala
+++ botDB/src/it/scala/com/benkio/botDB/ContainerSuite.scala
@@ -1,6 +1,7 @@
 package com.benkio.botDB
 
-import com.dimafeng.testcontainers.{DockerComposeContainer, ExposedService}
+import com.dimafeng.testcontainers.DockerComposeContainer
+import com.dimafeng.testcontainers.ExposedService
 import com.dimafeng.testcontainers.munit.TestContainerForAll
 import java.io.File
 import munit.Suite
@@ -10,6 +11,9 @@ trait ContainerSuite extends Suite with TestContainerForAll {
   val dbServiceName: String = "db_1"
   val dbServicePort: Int    = 3306
 
-  override val containerDef: DockerComposeContainer.Def = DockerComposeContainer.Def(new File("./docker-compose.yml"), exposedServices = Seq(ExposedService(dbServiceName, dbServicePort)))
+  override val containerDef: DockerComposeContainer.Def = DockerComposeContainer.Def(
+    new File("./docker-compose.yml"),
+    exposedServices = Seq(ExposedService(dbServiceName, dbServicePort))
+  )
 
 }
