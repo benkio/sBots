7e48e7237af568091f13f42acd677a217a83f510
Enrico Benini <benkio89@gmail.com>
2022-08-09 16:02:19 +0100

    Add `DBTimeout` Integration Tests

--- richardPHJBensonBot/src/it/scala/com/benkio/richardphjbensonbot/ITDBResourceAccessSpec.scala
+++ richardPHJBensonBot/src/it/scala/com/benkio/richardphjbensonbot/ITDBResourceAccessSpec.scala
@@ -1,5 +1,6 @@
 package com.benkio.richardphjbensonbot
 
+import telegramium.bots.ChatIntId
 import doobie.Transactor
 import cats.effect.Resource
 import log.effect.fs2.SyncLogWriter.consoleLog
@@ -11,6 +12,7 @@ import cats.effect.IO
 import com.dimafeng.testcontainers.DockerComposeContainer
 import munit._
 import cats.effect.unsafe.implicits.global
+import scala.concurrent.duration._
 
 import java.nio.file.Files
 
@@ -37,7 +39,8 @@ class ITDBResourceAccessSpec extends FunSuite with ContainerSuite {
       driver = "org.postgresql.Driver"
     )
 
-  test("getResourceByteArray should return the expected content") {
+  // DBResourceAccess tests
+  test("DBResourceAccess.getResourceByteArray should return the expected content") {
     withContainers { dockerComposeContainer =>
       val config     = buildConfig(dockerComposeContainer)
       val connection = buildDBConnection(dockerComposeContainer)
@@ -62,7 +65,7 @@ class ITDBResourceAccessSpec extends FunSuite with ContainerSuite {
         .unsafeRunSync()
     }
   }
-  test("getResourcesByKind should return the expected list of files with expected content") {
+  test("DBResourceAccess.getResourcesByKind should return the expected list of files with expected content") {
     withContainers { dockerComposeContainer =>
       val config     = buildConfig(dockerComposeContainer)
       val connection = buildDBConnection(dockerComposeContainer)
@@ -92,6 +95,112 @@ class ITDBResourceAccessSpec extends FunSuite with ContainerSuite {
         .unsafeRunSync()
     }
   }
+  // DBTimeout tests
+  test("DBTimeout.getOrDefault should return the default timeout if the chat id is not present in the database") {
+    withContainers { dockerComposeContainer =>
+      val config     = buildConfig(dockerComposeContainer)
+      val connection = buildDBConnection(dockerComposeContainer)
+      val transactor = Transactor.fromDriverManager[IO](
+        config.driver,
+        config.url,
+        config.user,
+        config.password,
+      )
+      val dbTimeout = DBTimeout[IO](transactor)
+      val actual = for {
+        _      <- Resource.eval(ITDBResourceAccessSpec.initDB(connection))
+        result <- Resource.eval(dbTimeout.getOrDefault(ChatIntId(100))) // Not present ChatID
+      } yield result
+      actual
+        .use { timeout =>
+          IO {
+            assertEquals(timeout.chat_id, "100")
+            assertEquals(timeout.timeout_value, "0")
+          }
+        }
+        .unsafeRunSync()
+    }
+  }
+  test("DBTimeout.getOrDefault should return the timeout if the chat id is present in the database") {
+    withContainers { dockerComposeContainer =>
+      val config     = buildConfig(dockerComposeContainer)
+      val connection = buildDBConnection(dockerComposeContainer)
+      val transactor = Transactor.fromDriverManager[IO](
+        config.driver,
+        config.url,
+        config.user,
+        config.password,
+      )
+      val dbTimeout = DBTimeout[IO](transactor)
+      val actual = for {
+        _      <- Resource.eval(ITDBResourceAccessSpec.initDB(connection))
+        result <- Resource.eval(dbTimeout.getOrDefault(ChatIntId(1))) // Present ChatID
+      } yield result
+      actual
+        .use { timeout =>
+          IO {
+            assertEquals(timeout.chat_id, "1")
+            assertEquals(timeout.timeout_value, "15000")
+          }
+        }
+        .unsafeRunSync()
+    }
+  }
+  test("DBTimeout.setTimeout should insert the timeout if the chat id is not present in the database") {
+    withContainers { dockerComposeContainer =>
+      val config     = buildConfig(dockerComposeContainer)
+      val connection = buildDBConnection(dockerComposeContainer)
+      val transactor = Transactor.fromDriverManager[IO](
+        config.driver,
+        config.url,
+        config.user,
+        config.password,
+      )
+      val dbTimeout = DBTimeout[IO](transactor)
+      val chatId    = ChatIntId(2)
+
+      val actual = for {
+        _      <- Resource.eval(ITDBResourceAccessSpec.initDB(connection))
+        _      <- Resource.eval(dbTimeout.setTimeout(chatId, 2.seconds))
+        result <- Resource.eval(dbTimeout.getOrDefault(chatId))
+      } yield result
+      actual
+        .use { timeout =>
+          IO {
+            assertEquals(timeout.chat_id, "2")
+            assertEquals(timeout.timeout_value, "2000")
+          }
+        }
+        .unsafeRunSync()
+    }
+  }
+  test("DBTimeout.setTimeout should update the timeout if the chat id is not present in the database") {
+    withContainers { dockerComposeContainer =>
+      val config     = buildConfig(dockerComposeContainer)
+      val connection = buildDBConnection(dockerComposeContainer)
+      val transactor = Transactor.fromDriverManager[IO](
+        config.driver,
+        config.url,
+        config.user,
+        config.password,
+      )
+      val dbTimeout = DBTimeout[IO](transactor)
+      val chatId    = ChatIntId(1)
+      val actual = for {
+        _      <- Resource.eval(ITDBResourceAccessSpec.initDB(connection))
+        _      <- Resource.eval(dbTimeout.setTimeout(chatId, 2.seconds))
+        result <- Resource.eval(dbTimeout.getOrDefault(chatId))
+      } yield result
+      actual
+        .use { timeout =>
+          IO {
+            assertEquals(timeout.chat_id, "1")
+            assertEquals(timeout.timeout_value, "2000")
+          }
+        }
+        .unsafeRunSync()
+    }
+  }
 }
 
 object ITDBResourceAccessSpec {
@@ -100,11 +209,13 @@ object ITDBResourceAccessSpec {
     """INSERT INTO media (media_name, kind, media_content, created_at) VALUES ('test media.mp3', NULL, decode('010203040506070809', 'hex'), '2008-01-01 00:00:01');""",
     """INSERT INTO media (media_name, kind, media_content, created_at) VALUES ('test media2.mp3', 'kind', decode('010203040506070809', 'hex'), '2009-01-01 00:00:01');""",
     """INSERT INTO media (media_name, kind, media_content, created_at) VALUES ('test media3.mp3', 'kind', decode('010203040506070809', 'hex'), '2010-01-01 00:00:01');""",
+    """INSERT INTO timeout (chat_id, timeout_value, last_interaction) VALUES ('1', '15000', '2010-01-01 00:00:01');""",
   )
 
   // Insert data in the DB
   def initDB(connection: Connection): IO[Unit] = IO {
     connection.createStatement().executeUpdate("DELETE FROM media;")
+    connection.createStatement().executeUpdate("DELETE FROM timeout;")
     inserts.foreach { insert =>
       connection.createStatement().executeUpdate(insert)
     }
--- richardPHJBensonBot/src/main/scala/com/benkio/richardphjbensonbot/DBTimeout.scala
+++ richardPHJBensonBot/src/main/scala/com/benkio/richardphjbensonbot/DBTimeout.scala
@@ -1,18 +1,20 @@
 package com.benkio.richardphjbensonbot
 
+import cats.Show
 import cats.effect.Async
 import cats.implicits._
 import doobie.implicits._
 import doobie.implicits.javasql._
-import doobie.{Transactor, _}
+import doobie.Transactor
+import doobie._
 import log.effect.LogWriter
-import telegramium.bots.{ChatId, ChatIntId, ChatStrId}
+import telegramium.bots.ChatId
+import telegramium.bots.ChatIntId
+import telegramium.bots.ChatStrId
 
 import java.sql.Timestamp
 import java.time.Instant
 import scala.concurrent.duration.FiniteDuration
-
-
 
 sealed trait DBTimeout[F[_]] {
   def getOrDefault(chatId: ChatId): F[DBTimeout.Timeout]
@@ -20,6 +22,14 @@ sealed trait DBTimeout[F[_]] {
 }
 
 object DBTimeout {
+
+  implicit val chatIdShow: Show[ChatId] = Show.show(chatId =>
+    chatId match {
+      case ChatIntId(id) => id.toString
+      case ChatStrId(id) => id
+    }
+  )
+
   final case class Timeout(chat_id: String, timeout_value: String, last_interaction: Timestamp)
 
   def defaultTimeout(chatId: ChatId): Timeout = Timeout(
@@ -39,24 +49,20 @@ object DBTimeout {
 
   private class DBTimeoutImpl[F[_]: Async](transactor: Transactor[F], log: LogWriter[F]) extends DBTimeout[F] {
 
-    private def chatIdFragment(chatId: ChatId): Fragment = chatId match {
-      case ChatIntId(id) => Fragment.const(id.toString)
-      case ChatStrId(id) => Fragment.const(id)
-    }
-
     private def getOrDefaultSql(chatId: ChatId): Query0[DBTimeout.Timeout] =
-      sql"SELECT chat_id, timeout_value, last_interaction FROM timeout WHERE chat_id = ${chatIdFragment(chatId)}".query[DBTimeout.Timeout]
+      sql"SELECT chat_id, timeout_value, last_interaction FROM timeout WHERE chat_id = ${chatId.show}"
+        .query[DBTimeout.Timeout]
 
     private def setTimeoutSql(chatId: ChatId, timeout: FiniteDuration): Update0 =
-      sql"INSERT INTO timeout (chat_id, timeout_value, last_interaction) VALUES (${chatIdFragment(chatId)}, ${new Timestamp(timeout.toMillis)}, NOW()::timestamp) ON CONFLICT (chat_id) DO UPDATE SET timeout_value = EXCLUDED.timeout_value, last_interaction = EXCLUDED.last_interaction".update
+      sql"INSERT INTO timeout (chat_id, timeout_value, last_interaction) VALUES (${chatId.show}, ${timeout.toMillis}, NOW()::timestamp) ON CONFLICT (chat_id) DO UPDATE SET timeout_value = EXCLUDED.timeout_value, last_interaction = EXCLUDED.last_interaction".update
 
     def getOrDefault(chatId: ChatId): F[DBTimeout.Timeout] =
       log.info(s"DB fetching timeout for $chatId") *>
-    getOrDefaultSql(chatId).option.transact(transactor).map(_.getOrElse(defaultTimeout(chatId)))
+        getOrDefaultSql(chatId).option.transact(transactor).map(_.getOrElse(defaultTimeout(chatId)))
 
     def setTimeout(chatId: ChatId, timeout: FiniteDuration): F[Unit] =
       log.info(s"DB setting timeout for $chatId to value $timeout") *>
-    setTimeoutSql(chatId, timeout).run.transact(transactor).void
+        setTimeoutSql(chatId, timeout).run.transact(transactor).void
   }
 
 }
