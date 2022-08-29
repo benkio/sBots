318afbb281aa09deabe7d5886fc04bd68116a437
Enrico Benini <benkio89@gmail.com>
2022-08-29 22:50:03 +0100

    `UrlFetcher` + Spec

--- richardPHJBensonBot/src/test/scala/com/benkio/richardphjbensonbot/UrlFetcherSpec.scala
+++ richardPHJBensonBot/src/test/scala/com/benkio/richardphjbensonbot/UrlFetcherSpec.scala
@@ -1,14 +1,31 @@
 package com.benkio.richardphjbensonbot
 
-import munit._
 
-class UrlFetcherSpec extends FunSuite {
+import munit.CatsEffectSuite
+import com.benkio.richardphjbensonbot.UrlFetcher
+import java.io.File
+import cats.effect._
+import java.net.MalformedURLException
+
+class UrlFetcherSpec extends CatsEffectSuite {
+
+  val urlFetcher = UrlFetcher[IO]()
 
   test("fetch should return the expected url content in a file if the url is valid") {
-    ???
+    val validUrl = "https://www.dropbox.com/s/mco2gb75ldfurvy/rphjb_06.gif?dl=1"
+    val filename = "rphjb_06.gif"
+    val actual: IO[Outcome[IO, Throwable, File]] = urlFetcher.fetch(validUrl, filename).flatMap(_.join)
+
+    assertIO(actual.map(_.isSuccess), true)
+
   }
 
   test("fetch should fail if the url is malformed") {
-    ???
+    val invalidUrl = "bad url"
+    val filename = "rphjb_06.gif"
+    val actual: IO[Outcome[IO, Throwable, File]] = urlFetcher.fetch(invalidUrl, filename).flatMap(_.join)
+
+    assertIO(actual.map(_.isError), true)
+    assertIO(actual.map(_.toString), Outcome.errored[IO,Throwable, File](new MalformedURLException("no protocol: bad url")).toString)
   }
 }
