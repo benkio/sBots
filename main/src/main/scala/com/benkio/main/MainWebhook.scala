97dae27ecb71f3a1353af4fd9a48500252b1808a
Enrico Benini <benkio89@gmail.com>
2022-09-08 17:41:36 +0100

    Add `youtuboAncheIoBot` to main

--- main/src/main/scala/com/benkio/main/MainWebhook.scala
+++ main/src/main/scala/com/benkio/main/MainWebhook.scala
@@ -9,6 +9,7 @@ import com.benkio.abarberobot.ABarberoBot
 import com.benkio.calandrobot.CalandroBot
 import com.benkio.richardphjbensonbot.RichardPHJBensonBot
 import com.benkio.xahbot.XahBot
+import com.benkio.youtuboancheiobot.YoutuboAncheIoBot
 import log.effect.fs2.SyncLogWriter.consoleLogUpToLevel
 import log.effect.LogLevels
 import log.effect.LogWriter
@@ -27,6 +28,10 @@ object MainWebhook extends IOApp {
         httpClient = httpClient,
         webhookBaseUrl = config.webhookBaseUrl
       )
+      youtuboAncheIoWebhook <- YoutuboAncheIoBot.buildWebhookBot[IO](
+        httpClient = httpClient,
+        webhookBaseUrl = config.webhookBaseUrl
+      )
       calandroWebhook <- CalandroBot.buildWebhookBot[IO](
         httpClient = httpClient,
         webhookBaseUrl = config.webhookBaseUrl
@@ -40,7 +45,7 @@ object MainWebhook extends IOApp {
         webhookBaseUrl = config.webhookBaseUrl
       )
       server <- WebhookBot.compose[IO](
-        bots = List(xahWebhook, calandroWebhook, richardPHJBensonWebhook, aBarberoWebhook),
+        bots = List(xahWebhook, calandroWebhook, richardPHJBensonWebhook, aBarberoWebhook, youtuboAncheIoWebhook),
         port = config.port,
         host = config.hostUrl
       )
