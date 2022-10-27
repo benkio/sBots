351546f67de5bc585e82bf9792cab3c0abe60ec2
Enrico Benini <benkio89@gmail.com>
2022-10-27 19:34:44 +0100

    Add `media_count` column to `media` table. botDB shouldn't overwrite the value

--- botDB/src/it/scala/com/benkio/botDB/DBSpec.scala
+++ botDB/src/it/scala/com/benkio/botDB/DBSpec.scala
@@ -38,12 +38,14 @@ class DBSpec extends CatsEffectSuite with DBConstants {
 
   databaseConnection.test("The `media` table should exist and be readable/writable") { connection =>
     connection.createStatement().executeUpdate(DBSpec.mediaSQL)
-    val resultSet = connection.createStatement().executeQuery("SELECT media_name, created_at FROM media")
+    val resultSet = connection.createStatement().executeQuery("SELECT media_name, created_at, media_count FROM media")
     resultSet.next()
     val actualMediaName      = resultSet.getString("media_name")
     val actualMediaCreatedAt = resultSet.getString("created_at")
+    val actualCount          = resultSet.getInt("media_count")
     assertEquals(actualMediaName, "test media.mp3")
     assertEquals(actualMediaCreatedAt, "2008-01-01 00:00:01")
+    assertEquals(actualCount, 0)
   }
 
   databaseConnection.test("The `timeout` table should exist and be readable/writable") { connection =>
@@ -70,7 +72,7 @@ class DBSpec extends CatsEffectSuite with DBConstants {
 object DBSpec {
 
   val mediaSQL: String = """
-INSERT INTO media (media_name, kind, media_url, created_at) VALUES ('test media.mp3', NULL, 'https://www.google.com', '2008-01-01 00:00:01');
+INSERT INTO media (media_name, kind, media_url, created_at, media_count) VALUES ('test media.mp3', NULL, 'https://www.google.com', '2008-01-01 00:00:01', 0);
 """
 
   val timeoutSQL = """
