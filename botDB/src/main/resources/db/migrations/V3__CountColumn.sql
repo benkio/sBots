d5350e80ed5748c9191abb40c590fad03dfdcc02
Enrico Benini <benkio89@gmail.com>
2022-10-28 14:40:17 +0100

    add migration

---
+++ botDB/src/main/resources/db/migrations/V3__CountColumn.sql
@@ -0,0 +1,22 @@
+PRAGMA foreign_keys=off;
+
+CREATE TABLE IF NOT EXISTS temp_media(
+  media_name TEXT PRIMARY KEY,
+  kind TEXT NULL,
+  media_url TEXT NOT NULL,
+  media_count INTEGER NOT NULL DEFAULT 0,
+  created_at TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS media_key
+ON temp_media(media_name);
+
+INSERT INTO temp_media(media_name, kind, media_url, media_count, created_at)
+SELECT media_name, kind, media_url, 0, created_at
+FROM media;
+
+DROP TABLE media;
+
+ALTER TABLE temp_media RENAME TO media; 
+
+PRAGMA foreign_keys=on;
