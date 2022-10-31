e5ede1875ae9b9d4ab3e47af42b7e3db0090a09b
Enrico Benini <benkio89@gmail.com>
2022-10-31 19:47:51 +0000

    Create `SubscribeUnsubscribeCommand` object for the descriptions and replyBundleCommand functions

--- telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/patterns/CommandPatterns.scala
+++ telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/patterns/CommandPatterns.scala
@@ -241,6 +241,22 @@ ${if (ignoreMessagePrefix.isDefined) {
       )
   }
 
+    object SubscribeUnsubscribeCommand {
+
+    val subscribeCommandDescriptionIta: String =
+      "'/subscribe 《cron time》': Iscrizione all'invio randomico di una puntata alla frequenza specificato nella chat corrente. Per il formato dell'input utilizzare questo sito come riferimento: https://crontab.guru"
+    val subscribeCommandDescriptionEng: String =
+      "'/subscribe 《cron time》': Subscribe to a random show at the specified frequency in the current chat. For the input format check the following site: https://crontab.guru"
+    val unsubscribeCommandDescriptionIta: String =
+      "'/unsubscribe': Disiscrizione della chat corrente dall'invio di puntate"
+    val unsubscribeCommandDescriptionEng: String =
+      "'/unsubscribe': UnSubscribe the current chat from random shows"
+
+    def subscribeReplyBundleCommand[F[_]: Async](): ReplyBundleCommand[F] = ???
+    def unsubscribeReplyBundleCommand[F[_]: Async](): ReplyBundleCommand[F] = ???
+
+  }
+
   def handleCommandWithInput[F[_]: Applicative](
       msg: Message,
       command: String,
