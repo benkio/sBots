c5d0e2aa9ce73f1aa52274b91a6dc48cda3b6ada
Enrico Benini <benkio89@gmail.com>
2022-11-01 13:44:34 +0000

    More DBMedia tests

--- telegramBotInfrastructure/src/it/scala/com/benkio/telegrambotinfrastructure/resources/db/ITDBResourceAccessSpec.scala
+++ telegramBotInfrastructure/src/it/scala/com/benkio/telegrambotinfrastructure/resources/db/ITDBResourceAccessSpec.scala
@@ -11,18 +11,25 @@ import cats.implicits._
 
 class ITDBResourceAccessSpec extends CatsEffectSuite with DBFixture {
 
-  val testMedia = "rphjb_MaSgus.mp3"
+  val testMediaName = "rphjb_MaSgus.mp3"
+  val testMedia: Media = Media(
+    testMediaName,
+    None,
+    "https://www.dropbox.com/sh/xqaatugvq8zcoyu/AACBnRH33traQAKBGy9bidu0a/rphjb_MaSgus.mp3?dl=1",
+    0,
+    "1662126019680"
+  )
 
   // DBResourceAccess tests
 
   databaseFixture.test("DBResourceAccess.getResourceByteArray should return the expected content") { fixture =>
     val resourceAssert = for {
       dbMedia          <- fixture.resourceDBMedia
-      preMedia         <- Resource.eval(dbMedia.getMedia(testMedia, false))
+      preMedia         <- Resource.eval(dbMedia.getMedia(testMediaName, false))
       dbResourceAccess <- fixture.resourceAccessResource
-      arrayContent     <- dbResourceAccess.getResourceByteArray(testMedia)
-      postMedia        <- Resource.eval(dbMedia.getMedia(testMedia, false))
-      _                <- Resource.eval(dbMedia.decrementMediaCount(testMedia))
+      arrayContent     <- dbResourceAccess.getResourceByteArray(testMediaName)
+      postMedia        <- Resource.eval(dbMedia.getMedia(testMediaName, false))
+      _                <- Resource.eval(dbMedia.decrementMediaCount(testMediaName))
     } yield {
       val assert1 = postMedia.media_count == (preMedia.media_count + 1)
       val assert2 = arrayContent.length >= (1024 * 5)
@@ -52,6 +59,64 @@ class ITDBResourceAccessSpec extends CatsEffectSuite with DBFixture {
   }
 
   // DBMedia
+
+  databaseFixture.test(
+    "DBMedia.getMedia should return the expected media"
+  ) { fixture =>
+    val resourceAssert = for {
+      dbMedia <- fixture.resourceDBMedia
+      media   <- Resource.eval(dbMedia.getMedia(filename = testMediaName))
+    } yield media == testMedia
+    resourceAssert.use(IO.pure).assert
+  }
+
+  databaseFixture.test(
+    "DBMedia.getMediaByKind should return the expected media list"
+  ) { fixture =>
+    val expected: List[Media] = List(
+      Media(
+        "ancheLaRabbiaHaUnCuore.txt",
+        Some("rphjb_LinkSources"),
+        "https://www.dropbox.com/sh/xqaatugvq8zcoyu/AABLDyXAOThfUrS3EoR3kL6ma/rphjb_LinkSources/ancheLaRabbiaHaUnCuore.txt?dl=1",
+        0,
+        "1662126021547"
+      ),
+      Media(
+        "live.txt",
+        Some("rphjb_LinkSources"),
+        "https://www.dropbox.com/sh/xqaatugvq8zcoyu/AACKI915JzajxuCSLy4spvbYa/rphjb_LinkSources/live.txt?dl=1",
+        0,
+        "1662126021551"
+      ),
+      Media(
+        "perCordeEGrida.txt",
+        Some("rphjb_LinkSources"),
+        "https://www.dropbox.com/sh/xqaatugvq8zcoyu/AAA6aMpu41wxHF3wFrYZTXGba/rphjb_LinkSources/perCordeEGrida.txt?dl=1",
+        0,
+        "1662126021555"
+      ),
+      Media(
+        "puntateCocktailMicidiale.txt",
+        Some("rphjb_LinkSources"),
+        "https://www.dropbox.com/sh/xqaatugvq8zcoyu/AAAfPoTfoPzhKys-DPI0YV8aa/rphjb_LinkSources/puntateCocktailMicidiale.txt?dl=1",
+        0,
+        "1662126021560"
+      ),
+      Media(
+        "puntateRockMachine.txt",
+        Some("rphjb_LinkSources"),
+        "https://www.dropbox.com/sh/xqaatugvq8zcoyu/AABSjYo7uJwDeQqKe3bA5cXea/rphjb_LinkSources/puntateRockMachine.txt?dl=1",
+        0,
+        "1662126021564"
+      )
+    )
+
+    val resourceAssert = for {
+      dbMedia <- fixture.resourceDBMedia
+      medias  <- Resource.eval(dbMedia.getMediaByKind(kind = "rphjb_LinkSources"))
+    } yield medias == expected
+    resourceAssert.use(IO.pure).assert
+  }
 
   databaseFixture.test(
     "DBMedia.getMediaByMediaCount should return the expected list of media"
