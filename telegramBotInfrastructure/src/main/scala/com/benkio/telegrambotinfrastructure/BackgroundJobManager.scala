5df425ea89b39e0f83f2009951a4ad6beec73b63
Enrico Benini <benkio89@gmail.com>
2022-11-14 20:17:41 +0000

    WIP `BackgroundJobManager`

--- telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/BackgroundJobManager.scala
+++ telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/BackgroundJobManager.scala
@@ -1,15 +1,59 @@
 package com.benkio.telegrambotinfrastructure
 
+import cats._
+import cats.effect._
+import cats.implicits._
 import com.benkio.telegrambotinfrastructure.model.Subscription
+import com.benkio.telegrambotinfrastructure.resources.db.DBSubscription
+import cron4s.expr.CronExpr
+import eu.timepit.fs2cron.Scheduler
+import eu.timepit.fs2cron.cron4s.Cron4sScheduler
+import fs2.Stream
+import fs2.concurrent.Signal
 import fs2.concurrent.SignallingRef
+
+import java.util.UUID
+import scala.collection.mutable.{ Map => MMap }
 
 trait BackgroundJobManager[F[_]] {
 
-  val memSubscriptions: Map[Int, SignallingRef[F, Boolean]]
+  var memSubscriptions: MMap[UUID, SignallingRef[F, Boolean]]
 
   def scheduleSubscription(subscription: Subscription): F[Unit]
   def loadSubscriptions(): F[Unit]
   def cancelSubscription(chatId: Int): F[Unit]
 }
 
-//TODO: Implement
+// TODO: Implement and Test
+object BackgroundJobManager {
+
+  def apply[F[_]](): F[BackgroundJobManager[F]] = ???
+
+  class BackgroundJobManagerImpl[F[_]: Async](dbSubscription: DBSubscription[F]) extends BackgroundJobManager[F] {
+    var memSubscriptions: MMap[UUID, SignallingRef[F, Boolean]] = MMap()
+
+    def scheduleSubscription(subscription: Subscription): F[Unit] = ???
+
+    def loadSubscriptions(): F[Unit] = for {
+      subscriptionsData <- dbSubscription.getSubscriptions()
+      subscriptions     <- subscriptionsData.traverse(s => MonadThrow[F].fromEither(Subscription(s)))
+      cronScheduler = Cron4sScheduler.utc[F]
+      subscriptionReferences <- subscriptions.traverse(s => BackgroundJobManager.runSubscription(s, cronScheduler))
+    } yield memSubscriptions = MMap.from(subscriptionReferences)
+
+    def cancelSubscription(chatId: Int): F[Unit] = ???
+  }
+
+  def runSubscription[F[_]: Async](
+      subscription: Subscription,
+      cronScheduler: Scheduler[F, CronExpr]
+  ): F[(UUID, SignallingRef[F, Boolean])] = {
+    val scheduled: Stream[F, Unit] = cronScheduler.awakeEvery(subscription.cron) >> ???
+    val cancel                     = SignallingRef[F, Boolean](false)
+
+    for {
+      c <- cancel
+      _ <- Async[F].start(scheduled.interruptWhen(c: Signal[F, Boolean]).repeat.compile.drain)
+    } yield (subscription.id, c)
+  }
+}
--- telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/model/Subscription.scala
+++ telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/model/Subscription.scala
@@ -1,10 +1,12 @@
 package com.benkio.telegrambotinfrastructure.model
 
+import com.benkio.telegrambotinfrastructure.resources.db.DBSubscriptionData
 import cron4s.Cron
 import cron4s.expr.CronExpr
 
 import java.time.Instant
 import java.util.UUID
+import scala.util.Try
 
 final case class Subscription(
     id: UUID,
@@ -21,6 +23,17 @@ object Subscription {
     chatId = chatId,
     cron = cron,
     subscribedAt = Instant.now()
+  )
+
+  def apply(dbSubscriptionData: DBSubscriptionData): Either[Throwable, Subscription] = for {
+    cron         <- Cron(dbSubscriptionData.cron)
+    id           <- Try(UUID.fromString(dbSubscriptionData.id)).toEither
+    subscribedAt <- Try(Instant.parse(dbSubscriptionData.subscribed_at)).toEither
+  } yield Subscription(
+    id = id,
+    chatId = dbSubscriptionData.chat_id,
+    cron = cron,
+    subscribedAt = subscribedAt
   )
 
 }
