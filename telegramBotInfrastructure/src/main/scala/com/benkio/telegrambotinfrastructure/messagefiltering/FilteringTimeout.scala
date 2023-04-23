7f3a3233682051f8d2c5aa512aa54fd5911c8a15
Enrico Benini <benkio89@gmail.com>
2023-04-23 13:10:41 +0100

    Recreate DB + format

--- telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/messagefiltering/FilteringTimeout.scala
+++ telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/messagefiltering/FilteringTimeout.scala
@@ -2,15 +2,16 @@ package com.benkio.telegrambotinfrastructure.messagefiltering
 
 import cats.MonadThrow
 import cats.implicits._
-import com.benkio.telegrambotinfrastructure.model.{ReplyBundleMessage, Timeout}
+import com.benkio.telegrambotinfrastructure.model.ReplyBundleMessage
+import com.benkio.telegrambotinfrastructure.model.Timeout
 import com.benkio.telegrambotinfrastructure.resources.db.DBLayer
 import telegramium.bots.Message
 
 object FilteringTimeout {
-  def filter[F[_]: MonadThrow](dbLayer: DBLayer[F], botName: String) : (ReplyBundleMessage[F], Message) => F[Boolean] =
+  def filter[F[_]: MonadThrow](dbLayer: DBLayer[F], botName: String): (ReplyBundleMessage[F], Message) => F[Boolean] =
     (_: ReplyBundleMessage[F], msg: Message) =>
-  for {
-    dbTimeout <- dbLayer.dbTimeout.getOrDefault(msg.chat.id, botName)
-    timeout   <- MonadThrow[F].fromEither(Timeout(dbTimeout))
-  } yield Timeout.isExpired(timeout)
+      for {
+        dbTimeout <- dbLayer.dbTimeout.getOrDefault(msg.chat.id, botName)
+        timeout   <- MonadThrow[F].fromEither(Timeout(dbTimeout))
+      } yield Timeout.isExpired(timeout)
 }
