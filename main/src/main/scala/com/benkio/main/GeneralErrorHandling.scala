0aa099e7d4b71737dd64a0617a1039f5b8ad6de1
Enrico Benini <benkio89@gmail.com>
2023-07-13 13:01:16 +0100

    format

--- main/src/main/scala/com/benkio/main/GeneralErrorHandling.scala
+++ main/src/main/scala/com/benkio/main/GeneralErrorHandling.scala
@@ -2,15 +2,19 @@ package com.benkio.main
 
 import cats.ApplicativeError
 import cats.effect.kernel.MonadCancel
-import cats.effect.{ExitCode, IO, Resource}
+import cats.effect.ExitCode
+import cats.effect.IO
+import cats.effect.Resource
 
 object GeneralErrorHandling {
 
-  def dbLogAndRestart[F[_], A](server: Resource[F, A])(implicit appErr: ApplicativeError[F,Throwable]): Resource[F, A] =
-    server.handleErrorWith((e : Throwable) => dbLogError(e).flatMap(_ => dbLogAndRestart(server)))
+  def dbLogAndRestart[F[_], A](server: Resource[F, A])(implicit
+      appErr: ApplicativeError[F, Throwable]
+  ): Resource[F, A] =
+    server.handleErrorWith((e: Throwable) => dbLogError(e).flatMap(_ => dbLogAndRestart(server)))
 
-  def dbLogAndRestart(app: IO[ExitCode])(implicit monCancel: MonadCancel[IO,Throwable]): IO[ExitCode] =
+  def dbLogAndRestart(app: IO[ExitCode])(implicit monCancel: MonadCancel[IO, Throwable]): IO[ExitCode] =
     app.handleErrorWith((e: Throwable) => dbLogError[IO](e).use_ *> app)
 
-  private def dbLogError[F[_]](e: Throwable) : Resource[F, Unit] = ???
+  private def dbLogError[F[_]](e: Throwable): Resource[F, Unit] = ???
 }
