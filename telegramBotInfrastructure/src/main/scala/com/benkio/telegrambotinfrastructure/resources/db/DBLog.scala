d1dd9ce24baf93bffbcf381953bf5170f8f7908c
Enrico Benini <benkio89@gmail.com>
2023-07-15 13:41:00 +0100

    Finished the `DBLog` and Test

--- telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/resources/db/DBLog.scala
+++ telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/resources/db/DBLog.scala
@@ -1,38 +1,50 @@
 package com.benkio.telegrambotinfrastructure.resources.db
 
-import java.time.Instant
 import cats.effect._
 import cats.implicits._
 import doobie._
 import doobie.implicits._
 
+import java.time.Instant
+
+final case class DBLogData(log_time: Long, message: String)
+
 trait DBLog[F[_]] {
   def writeLog(logMessage: String): F[Unit]
+  def getLastLog(): F[Option[DBLogData]]
 }
 
 object DBLog {
 
   def apply[F[_]: Async](
-    transactor: Transactor[F],
+      transactor: Transactor[F],
   ): DBLog[F] =
     new DBLogImpl[F](
-      transactor = transactor,
+      transactor = transactor
     )
 
   private[telegrambotinfrastructure] class DBLogImpl[F[_]: Async](
-    transactor: Transactor[F]
-  ) extends DBlog[F] {
+      transactor: Transactor[F]
+  ) extends DBLog[F] {
 
     override def writeLog(logMessage: String): F[Unit] = for {
       // get current time
       currentTime <- Async[F].delay(Instant.now().toEpochMilli)
       // truncate message to the max TEXT of SqlLite
-      m <- Async[F].pure(logMessage.substring(0, 2147483647))
+      m <- Async[F].pure(logMessage.take(2147483647))
       // Write the log
       _ <- writeLogQuery(currentTime, m).run.transact(transactor).void
     } yield ()
 
+    def getLastLog(): F[Option[DBLogData]] =
+      getLastLogQuery().option.transact(transactor)
+
     def writeLogQuery(logTime: Long, logMessage: String): Update0 =
       sql"INSERT INTO log (log_time, message) VALUES (${logTime}, ${logMessage})".update
+
+    def getLastLogQuery(): Query0[DBLogData] =
+      sql"SELECT log_time, message FROM log ORDER BY log_time DESC LIMIT 1"
+        .query[DBLogData]
+
   }
 }
