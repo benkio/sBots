704fe7fd03c430101681bfa6d6df8b1a903ad982
Scala Steward <scala_steward@virtuslab.com>
2023-10-16 17:10:57 +0000

    Update flyway-core to 9.22.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val emojiVersion       = "1.3.0"
     val fs2Core            = "3.9.2"
     val fs2IO              = "3.9.2"
-    val flyway             = "9.22.2"
+    val flyway             = "9.22.3"
     val fs2Cron            = "0.8.3"
     val http4s             = "0.23.23"
     val logEffects         = "0.17.0"
