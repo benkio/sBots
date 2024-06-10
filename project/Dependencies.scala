53c52f77aa3e9663dbf9613080ff6d381ffee4cd
Scala Steward <scala_steward@virtuslab.com>
2024-06-10 13:30:14 +0000

    Update pureconfig-core to 0.17.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val mulesHttp4s        = "0.4.0"
     val munit              = "1.0.0"
     val munitCatsEffect    = "2.0.0"
-    val pureConfig         = "0.17.6"
+    val pureConfig         = "0.17.7"
     val shapeless          = "2.3.10"
     val scalatest          = "3.2.16"
     val sqlite             = "3.41.2.1"
