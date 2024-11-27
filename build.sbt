5892dfc74b976663fa3873992d7a18c414f0d40e
Enrico Benini <benkio89@gmail.com>
2024-11-27 19:28:56 +0000

    387_SCoverage - Add TelegramReplySpec + mocks

--- build.sbt
+++ build.sbt
@@ -11,7 +11,7 @@ Global / onChangedBuildSource := ReloadOnSourceChanges
 // SCoverage
 coverageEnabled          := true
 coverageFailOnMinimum    := true
-coverageMinimumStmtTotal := 82 // TODO: INCREASE THIS
+coverageMinimumStmtTotal := 84 // TODO: INCREASE THIS
 
 // TASKS
 
---
+++ telegramBotInfrastructure/src/test/scala/com/benkio/telegrambotinfrastructure/mocks/ApiMock.scala
@@ -0,0 +1,30 @@
+package com.benkio.telegrambotinfrastructure.mocks
+
+import telegramium.bots.Chat
+import telegramium.bots.Message
+import cats.effect.IO
+import telegramium.bots.high.Api
+import telegramium.bots.client.Method
+import telegramium.bots.CirceImplicits.*
+
+import io.circe.*
+import io.circe.syntax.*
+
+class ApiMock extends Api[IO] {
+  def execute[Res](method: Method[Res]): IO[Res] =
+    IO(ApiMock.expectedResponses(method.payload.name))
+    .flatMap(json =>
+      IO.fromEither(method.decoder.decodeJson(json))
+    )
+}
+
+object ApiMock {
+  val expectedResponses : Map[String, Json]= Map(
+    "sendChatAction" -> Json.False,
+    "sendMessage" -> Message(0, date = 0, chat = Chat(0, `type` = "private"), text = Some("[apiMock] sendMessage reply")).asJson,
+    "sendVideo" -> Message(0, date = 0, chat = Chat(0, `type` = "private"), text = Some("[apiMock] sendVideo reply")).asJson,
+    "sendAudio" -> Message(0, date = 0, chat = Chat(0, `type` = "private"), text = Some("[apiMock] sendMp3 reply")).asJson,
+    "sendAnimation" -> Message(0, date = 0, chat = Chat(0, `type` = "private"), text = Some("[apiMock] sendGif reply")).asJson,
+    "sendPhoto" -> Message(0, date = 0, chat = Chat(0, `type` = "private"), text = Some("[apiMock] sendPhoto reply")).asJson,
+  )
+}
