8599073785dbb953293355e9dac4d5dbbb5c2c7b
Scala Steward <scala_steward@virtuslab.com>
2025-03-31 11:08:35 +0000

    Update munit-cats-effect to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val mules              = "0.7.0"
     val mulesHttp4s        = "0.4.0"
     val munit              = "1.1.0"
-    val munitCatsEffect    = "2.0.0"
+    val munitCatsEffect    = "2.1.0"
     val pureConfig         = "0.17.8"
     val shapeless          = "2.3.10"
     val scalatest          = "3.2.16"
