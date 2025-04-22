a3df22209c798802fe54324f407355ecceb7728e
Enrico Benini <benkio89@gmail.com>
2025-04-22 17:36:41 +0100

    685 - Copy the `media_count` from prod to base before replacing the data

--- DBMigration.sql
+++ DBMigration.sql
@@ -1,9 +1,21 @@
 ATTACH DATABASE 'botDB.sqlite3' AS prodDB;
 ATTACH DATABASE 'botDBBase.sqlite3' AS baseDB;
 
+UPDATE baseDB.media
+SET media_count = (
+    SELECT media_count
+    FROM prodDB.media
+    WHERE prodDB.media.media_name = baseDB.media.media_name
+)
+WHERE EXISTS (
+    SELECT 1 FROM prodDB.media WHERE prodDB.media.media_name = baseDB.media.media_name
+);
+
 DELETE FROM prodDB.media;
 DELETE FROM prodDB.show;
 
 INSERT INTO prodDB.media SELECT * FROM baseDB.media;
 INSERT INTO prodDB.show SELECT * FROM baseDB.show;
 
+DETACH DATABASE prodDB;
+DETACH DATABASE baseDB;
