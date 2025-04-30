dd4e8f68608315cc66bf1e061d8c6cbf0c81bf77
Enrico Benini <benkio89@gmail.com>
2025-04-30 13:36:59 +0100

    675-phase-1 - 👷 `MediaFileSourceGroup`

--- modules/bots/richardPHJBensonBot/src/main/scala/com/benkio/richardphjbensonbot/RichardPHJBensonBotMainDataEntry.scala
+++ modules/bots/richardPHJBensonBot/src/main/scala/com/benkio/richardphjbensonbot/RichardPHJBensonBotMainDataEntry.scala
@@ -1,5 +1,6 @@
 package com.benkio.richardphjbensonbot
 
+import com.benkio.telegrambotinfrastructure.model.MimeType
 import cats.effect.*
 import cats.syntax.all.*
 import com.benkio.telegrambotinfrastructure.model.media.MediaFileSource
@@ -18,15 +19,41 @@ object RichardPHJBensonBotMainDataEntry extends IOApp {
   val rphjbListFileResource =
     Resource.make(IO.delay(scala.io.Source.fromFile(rphjbListFilename)))(bufferedSorce => IO.delay(bufferedSorce.close))
 
+  case class MediaFileSourceGroup(
+    gif: Option[MediaFileSource] = None,
+    mp3: Option[MediaFileSource] = None,
+    vid: Option[MediaFileSource] = None
+  )
+
+  private def isGifMp4(mediaFileName: String): Boolean =
+    mediaFileName.dropRight(4).takeRight(3).toLowerCase == "gif"
+  private def getPrefix(mediaFileName: String): String =
+    if isGifMp4(mediaFileName)
+    then mediaFileName.dropRight(7)
+    else mediaFileName.dropRight(4)
+
+  private def toMediaFileSourceGroup(mediaFileSources: List[MediaFileSource]): List[MediaFileSourceGroup] =
+    mediaFileSources.groupBy(x => getPrefix(x.filename)).map { case (_, mediaFileSources: List[MediaFileSource]) =>
+      mediaFileSources.foldLeft(MediaFileSourceGroup()) {
+        case (mediaFileSourceGroup:MediaFileSourceGroup, mediaFileSource: MediaFileSource) =>
+          mediaFileSourceGroup.copy(
+            gif = if mediaFileSource.mime == MimeType.GIF then Some(mediaFileSource) else mediaFileSourceGroup.gif,
+            mp3 = if mediaFileSource.mime == MimeType.MPEG then Some(mediaFileSource) else mediaFileSourceGroup.mp3,
+            vid = if mediaFileSource.mime == MimeType.MP4 then Some(mediaFileSource) else  mediaFileSourceGroup.vid,
+          )
+      }
+    }.toList
+
   private def parseInput(links: List[String]): IO[List[MediaFileSource]] =
-    links.traverse(MediaFileSource.fromUriString)
+    links.traverse(link => MediaFileSource.fromUriString(link.replace("dl=0", "dl=1")))
 
   def run(args: List[String]): IO[ExitCode] =
     for
       _ <- IO.println(
         s"[RichardPHJBensonBotMainDataEntry:22:42]] Read the input ${args.length} links & parse them to Json"
       )
-      mediaFiles    <- parseInput(args).map(_.asJson)
+      mediafileSources     <- parseInput(args)
+      mediafileSourcesJson = mediafileSources.asJson
       _             <- IO.println("[RichardPHJBensonBotMainDataEntry:24:45]] Read the rphjb_list and parse it to json")
       rphjbListFile <- rphjbListFileResource.use(_.mkString.pure[IO])
       rphjbListFileJson <- IO.fromEither(parse(rphjbListFile))
@@ -34,15 +61,16 @@ object RichardPHJBensonBotMainDataEntry extends IOApp {
       mergedArray = {
         // Extract the arrays as lists of Json elements
         val elements1 = rphjbListFileJson.asArray.getOrElse(Vector.empty)
-        val elements2 = mediaFiles.asArray.getOrElse(Vector.empty)
+        val elements2 = mediafileSourcesJson.asArray.getOrElse(Vector.empty)
         // Combine and convert back to Json
         Json.fromValues(elements1 ++ elements2)
       }
       _ <- IO.println("[RichardPHJBensonBotMainDataEntry:34:45]] Write the json back")
       _ <- IO(Files.write(Paths.get(rphjbListFilename), mergedArray.toString.getBytes(StandardCharsets.UTF_8)))
 
+      mediaFileSourceGroups = toMediaFileSourceGroup(mediafileSources)
+      _ <- IO.println(s"[RichardPHJBensonBotMainDataEntry:71:45]] groups: $mediaFileSourceGroups")
     // For Scala Code Gen
-    // - Group by prefix (eg. take the mp3 or mp4)
     // - Convert groups to reply bundle message by content (mix, only audio, only gif)
     // - return the scala code
     yield ExitCode.Success
