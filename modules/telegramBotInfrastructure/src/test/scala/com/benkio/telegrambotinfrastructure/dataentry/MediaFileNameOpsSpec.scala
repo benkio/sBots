9dbcbf1681f2d6148f5e40d4fb6e2579f904869f
Enrico Benini <benkio89@gmail.com>
2025-05-01 14:18:11 +0100

    675-phase-extra - ✓ `MediaFileNameOpsSpec.scala`

--- modules/telegramBotInfrastructure/src/test/scala/com/benkio/telegrambotinfrastructure/dataentry/MediaFileNameOpsSpec.scala
+++ modules/telegramBotInfrastructure/src/test/scala/com/benkio/telegrambotinfrastructure/dataentry/MediaFileNameOpsSpec.scala
@@ -4,8 +4,21 @@ import munit.*
 
 class MediaFileNameOpsSpec extends FunSuite {
   test("getPrefix should return the expected prefix") {
-    val actual   = 42
-    val expected = 43
-    assertEquals(actual, expected)
+    val test = List(
+      "rphjb_06.mp4"                                    -> "rphjb_06",
+      "rphjb_06Gif.mp4"                                 -> "rphjb_06",
+      "rphjb_2Orecchie1Bocca.mp4"                       -> "rphjb_2Orecchie1Bocca",
+      "rphjb_2Orecchie1Bocca2.mp4"                      -> "rphjb_2Orecchie1Bocca2",
+      "rphjb_3Minuti.mp4"                               -> "rphjb_3Minuti",
+      "rphjb_4SoloTempiInTestaOrologiSfuggonoPolsi.mp4" -> "rphjb_4SoloTempiInTestaOrologiSfuggonoPolsi",
+      "rphjb_5DitaRivolta.mp3"                          -> "rphjb_5DitaRivolta",
+      "rphjb_5DitaRivolta.mp4"                          -> "rphjb_5DitaRivolta",
+      "rphjb_5DitaRivoltaGif.mp4"                       -> "rphjb_5DitaRivolta",
+      "rphjb_911TorriGemelleBinLaden.mp3"               -> "rphjb_911TorriGemelleBinLaden",
+      "rphjb_911TorriGemelleBinLaden.mp4"               -> "rphjb_911TorriGemelleBinLaden"
+    )
+    test.foreach { case (input, expected) =>
+      assertEquals(MediaFileNameOps.getPrefix(input), expected)
+    }
   }
 }
