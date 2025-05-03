5a669dc7e2a48bbd5768f4968eb26bd6ce35f527
Enrico Benini <benkio89@gmail.com>
2025-05-03 13:42:23 +0100

    694 - `fmt` + fix `deploy` because of this: https://stackoverflow.com/a/76294014

--- modules/telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/patterns/CommandPatternsGroup.scala
+++ modules/telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/patterns/CommandPatternsGroup.scala
@@ -1,52 +1,42 @@
 package com.benkio.telegrambotinfrastructure.patterns
 
+import cats.effect.Async
+import com.benkio.telegrambotinfrastructure.model.reply.ReplyBundleCommand
 import com.benkio.telegrambotinfrastructure.model.reply.ReplyBundleMessage
 import com.benkio.telegrambotinfrastructure.patterns.CommandPatterns.*
-import com.benkio.telegrambotinfrastructure.model.reply.ReplyBundleCommand
-import cats.effect.Async
-
-
-
-
-
 import com.benkio.telegrambotinfrastructure.resources.db.*
 import com.benkio.telegrambotinfrastructure.BackgroundJobManager
-
-
-
 import log.effect.LogWriter
 import org.http4s.Uri
-
 
 object CommandPatternsGroup {
 
   object ShowGroup {
 
     def group[F[_]: Async](
-      dbShow: DBShow[F],
-      dbSubscription: DBSubscription[F],
-      backgroundJobManager: BackgroundJobManager[F],
-      botName: String
+        dbShow: DBShow[F],
+        dbSubscription: DBSubscription[F],
+        backgroundJobManager: BackgroundJobManager[F],
+        botName: String
     )(using log: LogWriter[F]): List[ReplyBundleCommand[F]] =
       List(
         SearchShowCommand.searchShowReplyBundleCommand(dbShow, botName),
         SubscribeUnsubscribeCommand.subscribeReplyBundleCommand(backgroundJobManager, botName),
         SubscribeUnsubscribeCommand.unsubscribeReplyBundleCommand(backgroundJobManager, botName),
-        SubscribeUnsubscribeCommand.subscriptionsReplyBundleCommand(dbSubscription, backgroundJobManager, botName),
-
-    )
+        SubscribeUnsubscribeCommand.subscriptionsReplyBundleCommand(dbSubscription, backgroundJobManager, botName)
+      )
   }
 
   object TriggerGroup {
     def group[F[_]: Async](
-      triggerFileUri: Uri,
-      botName: String,
-      ignoreMessagePrefix: Option[String],
-      messageRepliesData: List[ReplyBundleMessage[F]],
-      botPrefix: String,
-      dbMedia: DBMedia[F],
-      dbTimeout: DBTimeout[F],
-    )(using log: LogWriter[F]) : List[ReplyBundleCommand[F]] =
+        triggerFileUri: Uri,
+        botName: String,
+        ignoreMessagePrefix: Option[String],
+        messageRepliesData: List[ReplyBundleMessage[F]],
+        botPrefix: String,
+        dbMedia: DBMedia[F],
+        dbTimeout: DBTimeout[F]
+    )(using log: LogWriter[F]): List[ReplyBundleCommand[F]] =
       List(
         TriggerListCommand.triggerListReplyBundleCommand(triggerFileUri),
         TriggerSearchCommand.triggerSearchReplyBundleCommand(botName, ignoreMessagePrefix, messageRepliesData),
