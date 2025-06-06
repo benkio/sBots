75a5f67aa651d73b81b2813599026ae42a12e604
Enrico Benini <benkio89@gmail.com>
2025-06-06 18:35:42 +0100

    631 - Disable `MainSpec` for CI

--- modules/botDB/src/test/scala/com/benkio/botDB/MainSpec.scala
+++ modules/botDB/src/test/scala/com/benkio/botDB/MainSpec.scala
@@ -5,7 +5,12 @@ import munit.*
 
 class MainSpec extends CatsEffectSuite {
 
+  val ciEnvVar = sys.env.get("CI")
+
   test("Main.Initialization should not raise exception when inizialize the module") {
+    // Disable for CI as there's no YoutubeApiKey file
+    assume(ciEnvVar.contains("false") || ciEnvVar.isEmpty)
+
     assertIO_(Main.initialization(List.empty).use_)
   }
 }
