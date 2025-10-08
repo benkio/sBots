53605e06182c173454e646d6ca762cd8afc320dd
Enrico Benini <workemail>
2025-10-08 19:28:10 +0100

    ➕ Title check for mp3s

--- filesCheck/src/id3Functions.ts
+++ filesCheck/src/id3Functions.ts
@@ -2,8 +2,11 @@ import * as NodeID3 from 'node-id3';
 import * as path from 'node:path';
 import logger from './logger';
 
-export function fixMp3ArtistId3Tag(file: string, initialArtist: string): void {
-  const artistTag = NodeID3.read(file).artist;
+function checkArtist(
+  initialArtist: string,
+  artistTag: string | undefined,
+  file: string
+): void {
   if (artistTag !== initialArtist) {
     logger.info(
       `[id3Functions] Update file ${path.basename(file)} with artist ${initialArtist}`
@@ -15,12 +18,25 @@ export function fixMp3ArtistId3Tag(file: string, initialArtist: string): void {
       logger.info('[id3Functions] Tag not updated: operation failed');
     }
   } else {
-    if (artistTag === initialArtist) {
-      logger.verbose(`[id3Functions] ✓ ${path.basename(file)}`);
-    } else {
+    if (artistTag !== initialArtist) {
       logger.warn(
         `[id3Functions] ⚠️ ${path.basename(file)} not updated: ${artistTag}(file) !== ${initialArtist}(wanted)`
       );
     }
   }
+}
+
+function checkTitle(file: string, title: string | undefined): void {
+  if (!title) {
+    console.error(
+      `[id3Functions] 🚫 ${path.basename(file)} doesn't contain a title`
+    );
+  }
+}
+
+export function fixMp3ArtistId3Tag(file: string, initialArtist: string): void {
+  const tags = NodeID3.read(file);
+  checkArtist(initialArtist, tags.artist, file);
+  checkTitle(file, tags.title);
+  logger.verbose(`[id3Functions] ✓ ${path.basename(file)}`);
 }
