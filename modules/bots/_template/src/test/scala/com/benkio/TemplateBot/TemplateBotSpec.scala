b6821adc3a1b8bc8d3271d333b4ef120c323559c
Enrico Benini <benkio89@gmail.com>
2026-02-21 15:12:29 +0000

    ➕ template test and improve test speed

---
+++ modules/bots/_template/src/test/scala/com/benkio/TemplateBot/TemplateBotSpec.scala
@@ -0,0 +1,104 @@
+package com.benkio.TemplateBot
+
+import cats.data.NonEmptyList
+import cats.effect.Async
+import cats.effect.IO
+import cats.syntax.all.*
+import cats.Parallel
+import cats.Show
+import com.benkio.telegrambotinfrastructure.mocks.ApiMock.given
+import com.benkio.telegrambotinfrastructure.mocks.DBLayerMock
+import com.benkio.telegrambotinfrastructure.mocks.RepositoryMock
+import com.benkio.telegrambotinfrastructure.model.media.MediaResource.MediaResourceIFile
+import com.benkio.telegrambotinfrastructure.model.reply.Document
+import com.benkio.telegrambotinfrastructure.model.reply.MediaFile
+import com.benkio.telegrambotinfrastructure.model.reply.ReplyBundleCommand
+import com.benkio.telegrambotinfrastructure.model.reply.ReplyBundleMessage
+import com.benkio.telegrambotinfrastructure.model.Trigger
+import com.benkio.telegrambotinfrastructure.repository.db.DBLayer
+import com.benkio.telegrambotinfrastructure.repository.Repository.RepositoryError
+import com.benkio.telegrambotinfrastructure.repository.ResourcesRepository
+import com.benkio.telegrambotinfrastructure.config.SBotConfig
+import com.benkio.telegrambotinfrastructure.BaseBotSpec
+import com.benkio.telegrambotinfrastructure.SBot
+import com.benkio.telegrambotinfrastructure.SBotPolling
+import log.effect.fs2.SyncLogWriter.consoleLogUpToLevel
+import log.effect.LogLevels
+import log.effect.LogWriter
+import munit.CatsEffectSuite
+
+class TemplateBotSpec extends BaseBotSpec {
+
+  given log: LogWriter[IO]                   = consoleLogUpToLevel(LogLevels.Info)
+  val tplSBotConfig: SBotConfig = SBot.buildSBotConfig(TemplateBot.sBotInfo)
+  val emptyDBLayer: DBLayer[IO]              = DBLayerMock.mock(tplSBotConfig.sBotInfo.botId)
+  val mediaResource: MediaResourceIFile[IO] =
+    MediaResourceIFile(
+      "test mediafile"
+    )
+  val repositoryMock = new RepositoryMock(
+    getResourceByKindHandler = (_, botId) =>
+      IO.raiseUnless(botId == tplSBotConfig.sBotInfo.botId)(
+        Throwable(s"[TemplateBotSpec] getResourceByKindHandler called with unexpected botId: $botId")
+      ).as(NonEmptyList.one(NonEmptyList.one(mediaResource))),
+    getResourceFileHandler = (mediaFile: MediaFile) =>
+      mediaFile match {
+        case Document(v, _) if v == tplSBotConfig.repliesJsonFilename =>
+          ResourcesRepository.fromResources[IO]().getResourceFile(mediaFile).use(IO.pure)
+        case _ => Left(RepositoryError.NoResourcesFoundFile(mediaFile)).pure[IO]
+      }
+  )
+
+  val templateBot: IO[SBotPolling[IO]] = for {
+    botSetup <- buildTestBotSetup(
+      repository = repositoryMock,
+      dbLayer = emptyDBLayer,
+      sBotConfig = tplSBotConfig,
+      ttl = tplSBotConfig.messageTimeToLive
+    )
+    messageRepliesData <- botSetup.jsonDataRepository.loadData[ReplyBundleMessage](
+      tplSBotConfig.repliesJsonFilename
+    )
+    commandRepliesData <- botSetup.jsonDataRepository.loadData[ReplyBundleCommand](
+      tplSBotConfig.commandsJsonFilename
+    )
+  } yield new SBotPolling[IO](botSetup, messageRepliesData, commandRepliesData)(using
+    Parallel[IO],
+    Async[IO],
+    botSetup.api,
+    log
+  )
+
+  val messageRepliesData: IO[List[ReplyBundleMessage]] =
+    templateBot.map(_.messageRepliesData)
+  val commandRepliesData: IO[List[ReplyBundleCommand]] =
+    templateBot.map(_.allCommandRepliesData)
+  val messageRepliesDataPrettyPrint: IO[List[String]] =
+    messageRepliesData.map(_.flatMap(mr => mr.reply.prettyPrint))
+
+  messageRepliesData
+    .map(mrd => {
+      exactTriggerReturnExpectedReplyBundle(mrd)
+    })
+    .unsafeRunSync()
+
+  test("TemplateBot should contain the expected number of commands") {
+    assertIO(
+      commandRepliesData.map(_.length),
+      5,
+      "TemplateBot should have 5 commands"
+    )
+  }
+
+  jsonContainsFilenames(
+    jsonFilename = "tpl_list.json",
+    botData = messageRepliesDataPrettyPrint
+  )
+  botJsonsAreValid(tplSBotConfig)
+
+  triggerFileContainsTriggers(
+    triggerFilename = tplSBotConfig.triggerFilename,
+    botMediaFiles = messageRepliesDataPrettyPrint,
+    botTriggersIO = messageRepliesData.map(_.flatMap(mrd => Show[Trigger].show(mrd.trigger).split('\n')))
+  )
+}
