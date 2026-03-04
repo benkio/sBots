4d95206127ae3ff9d0a0a65a7bffffd07d0dacbf
Scala Steward <scala_steward@virtuslab.com>
2026-03-04 16:13:06 +0000

    Update munit to 1.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     val logbackLogstash       = "9.0"
     val mules                 = "0.7.0"
     val mulesHttp4s           = "0.4.0"
-    val munit                 = "1.2.0"
+    val munit                 = "1.2.4"
     val munitCatsEffect       = "2.1.0"
     val pureConfig            = "0.17.10"
     val scalajsDom            = "2.8.1"
