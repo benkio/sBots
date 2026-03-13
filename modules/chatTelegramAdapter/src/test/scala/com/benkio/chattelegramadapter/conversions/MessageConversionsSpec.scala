e7c8c16a401145e52b73b3072f39dba8884a22d9
Enrico Benini <benkio89@gmail.com>
2026-03-13 14:39:53 +0000

    ➕ `MessageConversionsSpec`

--- modules/chatTelegramAdapter/src/test/scala/com/benkio/chattelegramadapter/conversions/MessageConversionsSpec.scala
+++ modules/chatTelegramAdapter/src/test/scala/com/benkio/chattelegramadapter/conversions/MessageConversionsSpec.scala
@@ -1,9 +1,28 @@
 package com.benkio.chattelegramadapter.conversions
 
+import com.benkio.chattelegramadapter.conversions.MessageConversions.toModelMessage
+import com.benkio.chattelegramadapter.Arbitraries.given
 import munit.FunSuite
+import munit.ScalaCheckSuite
+import org.scalacheck.Prop.*
+import telegramium.bots.InaccessibleMessage
+import telegramium.bots.MaybeInaccessibleMessage
+import telegramium.bots.Message as TelegramMessage
 
-class MessageConversionsSpec extends FunSuite {
+class MessageConversionsSpec extends ScalaCheckSuite {
   test("toModelMessage should convert MaybeInaccessibleMessage to core Message when possible") {
-    assert(false)
+    {
+      forAll { (maybeInaccessibleMessage: MaybeInaccessibleMessage) =>
+        val message = maybeInaccessibleMessage.toModelMessage
+        assert(message.isDefined)
+        assertEquals(
+          message.get.messageId,
+          maybeInaccessibleMessage match {
+            case msg: TelegramMessage     => msg.messageId
+            case msg: InaccessibleMessage => msg.messageId
+          }
+        )
+      }
+    }
   }
 }
