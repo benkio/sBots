f68c571f5ac5d994406fc7bb45abe7c5a0dca5e7
Enrico Benini <benkio89@gmail.com>
2026-04-24 18:01:12 +0100

    Move logic to the telegram adapter

--- modules/main/src/main/scala/com/benkio/main/MainWebhook.scala
+++ modules/main/src/main/scala/com/benkio/main/MainWebhook.scala
@@ -4,11 +4,9 @@ import cats.effect.ExitCode
 import cats.effect.IO
 import cats.effect.IOApp
 import cats.effect.Resource
-import com.benkio.chattelegramadapter.http.LogTelegramChat
 import com.benkio.chattelegramadapter.webhook.TelegramWebhookServer
 import com.benkio.main.Logger.given
 import org.http4s.server.Server
-import telegramium.bots.high.Api
 
 object MainWebhook extends IOApp {
 
@@ -22,17 +20,6 @@ object MainWebhook extends IOApp {
         host = mainSetup.host,
         keystorePath = mainSetup.keystorePath,
         keystorePassword = mainSetup.keystorePassword
-      )
-      _ <- Resource.eval(
-        bots.foldLeft(IO.unit) { (acc, bot) =>
-          acc >> {
-            given Api[IO] = bot.sBotSetup.api
-            LogTelegramChat.sendText(
-              msg = "Start Webook Bot Successful ✅",
-              sBotInfo = bot.sBotSetup.sBotConfig.sBotInfo
-            )
-          }
-        }
       )
     } yield server
 
