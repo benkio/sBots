4e8333e20c6785af2ba9b049e13b8817f3562c95
Enrico Benini <benkio89@gmail.com>
2026-06-25 15:49:58 +0100

    Run dbSetup for all bots with srt saving

--- modules/botDB/src/main/scala/com/benkio/botDB/show/ShowUpdater.scala
+++ modules/botDB/src/main/scala/com/benkio/botDB/show/ShowUpdater.scala
@@ -210,7 +210,7 @@ object ShowUpdater {
         showFilesResource.use(showFiles =>
           for {
             _ <- LogWriter.info(s"[ShowUpdater] ✓ Dry Run. Delete show files: ${showFiles.map(_.file)}")
-            _ <- showFiles.traverse(showFile => Async[F].delay(Files.delete(showFile.file)))
+            _ <- showFiles.traverse_(showFile => Async[F].delay(Files.delete(showFile.file))).handleErrorWith(e => LogWriter.error(s"[ShowUpdater] 🚫 Error in delete file: ${e.getMessage()}"))
           } yield List.empty
         )
       val getStoredDbShowDatas: F[List[YouTubeBotDBShowDatas]] =
