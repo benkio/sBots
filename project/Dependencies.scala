d2a06ccf8a9676c38843ef00b440b65ece8b2980
Scala Steward <scala_steward@virtuslab.com>
2026-07-12 15:27:06 +0000

    Update google-api-services-youtube to v3-rev20260709-2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val googleApiClient       = "2.9.0"
     val googleHttpClient      = "2.1.1"
     val googleOauthClient     = "1.39.0"
-    val googleYouTubeApi      = "v3-rev20260707-2.0.0"
+    val googleYouTubeApi      = "v3-rev20260709-2.0.0"
     val http4s                = "0.23.36"
     val ip4s                  = "3.8.0"
     val logEffects            = "0.19.9"
