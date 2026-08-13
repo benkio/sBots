cab51f04013bc4bdf8fe1b403fdb930f1e186df8
Scala Steward <scala_steward@virtuslab.com>
2026-08-13 15:43:41 +0000

    Update ujson to 4.3.2

--- scripts/CheckMegaLinks.scala
+++ scripts/CheckMegaLinks.scala
@@ -2,7 +2,7 @@
 //> using scala "3"
 //> using toolkit typelevel:default
 //> using dep "com.lihaoyi::os-lib:0.11.8"
-//> using dep "com.lihaoyi::ujson:4.3.1"
+//> using dep "com.lihaoyi::ujson:4.3.2"
 
 import java.net.URI
 import java.net.http.HttpClient
